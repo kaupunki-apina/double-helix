7eec9baad526ff3d014cad69bdf99739b3c61250
Tomy Salminen <tomy.salminen@futurice.com>
2019-02-26 22:18:59 +0200

    Colored translucent statusbar

--- app/src/main/java/fi/tomy/salminen/doublehelix/core/BaseViewModel.kt
+++ app/src/main/java/fi/tomy/salminen/doublehelix/core/BaseViewModel.kt
@@ -1,7 +1,13 @@
 package fi.tomy.salminen.doublehelix.core
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseViewModel : ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
 
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
--- app/src/main/java/fi/tomy/salminen/doublehelix/feature/feed/FeedActivityViewModel.kt
+++ app/src/main/java/fi/tomy/salminen/doublehelix/feature/feed/FeedActivityViewModel.kt
@@ -2,18 +2,12 @@ package fi.tomy.salminen.doublehelix.feature.feed
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import io.reactivex.disposables.CompositeDisposable
+import fi.tomy.salminen.doublehelix.core.BaseViewModel
 
 
-class FeedActivityViewModel : ViewModel() {
-    private val compositeDisposable = CompositeDisposable()
+class FeedActivityViewModel : BaseViewModel() {
 
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposable.clear()
-    }
-
-    class Factory() : ViewModelProvider.Factory {
+    class Factory : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return FeedActivityViewModel() as T
         }
