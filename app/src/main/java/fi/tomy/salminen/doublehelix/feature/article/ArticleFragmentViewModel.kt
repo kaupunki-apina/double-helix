4c277a79e709a15216d19fc091ae3dc955a00241
Tomy Salminen <tomy.salminen@futurice.com>
2019-05-12 10:28:23 +0300

    Open links in custom chrome tabs instead of webview

--- app/src/main/java/fi/tomy/salminen/doublehelix/feature/article/ArticleFragmentViewModel.kt
+++ app/src/main/java/fi/tomy/salminen/doublehelix/feature/article/ArticleFragmentViewModel.kt
@@ -1,8 +1,6 @@
 package fi.tomy.salminen.doublehelix.feature.article
 
 import android.app.Application
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import fi.tomy.salminen.doublehelix.feature.viewmodel.BaseContextViewModel
@@ -12,16 +10,6 @@ class ArticleFragmentViewModel(
     private val articleRepository: ArticleRepository,
     app: Application
 ) : BaseContextViewModel(app) {
-
-    private val mutableUrl: MutableLiveData<String> = MutableLiveData()
-    val url: LiveData<String> get() = mutableUrl
-
-    fun init(articleId: Int) {
-        compositeDisposable.add(
-            articleRepository.getArticleById(articleId)
-                .doOnSuccess { mutableUrl.postValue(it.link) }
-                .subscribe())
-    }
 
     class Factory(val articleRepository: ArticleRepository, val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
