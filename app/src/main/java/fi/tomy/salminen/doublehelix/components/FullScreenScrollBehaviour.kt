041efe84af52469afd253c2887673511b8386cd0
Unknown <tomy_salminen@msn.com>
2019-07-31 13:17:26 +0300

    [WIP] Progress on layout behaviour

--- app/src/main/java/fi/tomy/salminen/doublehelix/components/FullScreenScrollBehaviour.kt
+++ app/src/main/java/fi/tomy/salminen/doublehelix/components/FullScreenScrollBehaviour.kt
@@ -3,11 +3,12 @@ package fi.tomy.salminen.doublehelix.components
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
+import androidx.customview.widget.ViewDragHelper
 import fi.tomy.salminen.doublehelix.R
-import kotlin.math.max
 
 
 class FullScreenScrollBehaviour(context: Context, attrs: AttributeSet) :
@@ -17,8 +18,37 @@ class FullScreenScrollBehaviour(context: Context, attrs: AttributeSet) :
         val NO_ID = -1
     }
 
+    // val STATE_RESTING = 1
+    // val STATE_DRAGGING = 2
+    // var dragState = STATE_RESTING
     val anchorId: Int
     var consumalbeOffset: Int? = null
+    /*
+    var viewDragHelper: ViewDragHelper? = null
+    val viewDragHelperCallback = object : ViewDragHelper.Callback() {
+        override fun tryCaptureView(child: View, pointerId: Int): Boolean {
+            return true
+        }
+
+        override fun onViewDragStateChanged(state: Int) {
+            if (state == ViewDragHelper.STATE_DRAGGING) {
+                dragState = STATE_DRAGGING
+            }
+        }
+
+        override fun onViewReleased(releasedChild: View, xvel: Float, yvel: Float) {
+            dragState = STATE_RESTING
+        }
+
+        override fun clampViewPositionVertical(child: View, top: Int, dy: Int): Int {
+            return top
+        }
+
+        override fun getViewVerticalDragRange(child: View): Int {
+            return -200
+        }
+    }
+    */
 
     init {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.View)
@@ -63,7 +93,6 @@ class FullScreenScrollBehaviour(context: Context, attrs: AttributeSet) :
         val anchor = getAnchor(coordinatorLayout)
         return anchor != null && axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
-
     /*
     override fun onNestedScroll(
         coordinatorLayout: CoordinatorLayout,
@@ -76,32 +105,47 @@ class FullScreenScrollBehaviour(context: Context, attrs: AttributeSet) :
         type: Int,
         consumed: IntArray
     ) {
+        // Handles scrolling from upper bound of the screen down anchor bottom
         val anchor = getAnchor(coordinatorLayout) ?: return
-        val offset: Int
 
-        if (target.scrollY > anchor.bottom - child.top) {
-            offset = anchor.bottom - child.top
-            return super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed)
-        }
-
-        val maxOffset: Int
-
-        if (dyConsumed > 0) {
-            maxOffset = anchor.top - child.top
-            offset = Math.max(maxOffset, -dyConsumed)
-
-        } else {
-            maxOffset = anchor.bottom - child.top
-            offset = Math.min(maxOffset, -dyConsumed)
-        }
-
-        if (offset == 0) {
-            super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed)
-        } else {
-            consumed[1] = offset
+        if (dyUnconsumed < 0) {
+            val maxOffset = anchor.bottom - child.top
+            val offset = Math.max(maxOffset, dyUnconsumed)
             consumalbeOffset = offset
+            consumed[1] = offset
             ViewCompat.offsetTopAndBottom(child, offset)
         }
+
+        super.onNestedScroll(
+            coordinatorLayout,
+            child,
+            target,
+            dxConsumed,
+            dyConsumed,
+            dxUnconsumed,
+            dyUnconsumed,
+            type,
+            consumed
+        )
+    }
+
+    override fun onTouchEvent(parent: CoordinatorLayout, child: View, event: MotionEvent): Boolean {
+        if (!child.isShown) {
+            return false
+        }
+
+        val action = event.actionMasked
+
+        if (action == MotionEvent.ACTION_MOVE) {
+            if (viewDragHelper == null) {
+                viewDragHelper = ViewDragHelper.create(parent, viewDragHelperCallback)
+            }
+
+            viewDragHelper!!.processTouchEvent(event)
+            return viewDragHelper!!.touchSlop < event.y
+        }
+
+        return false
     }
     */
 }
