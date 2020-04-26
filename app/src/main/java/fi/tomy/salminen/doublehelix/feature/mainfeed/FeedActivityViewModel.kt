2eb93b86f7728893567bc76a388554e0fcb40453
Unknown <tomy_salminen@msn.com>
2020-04-26 16:06:31 +0300

    Fetch main feed when VM is created

--- app/src/main/java/fi/tomy/salminen/doublehelix/feature/mainfeed/FeedActivityViewModel.kt
+++ app/src/main/java/fi/tomy/salminen/doublehelix/feature/mainfeed/FeedActivityViewModel.kt
@@ -1,8 +1,18 @@
 package fi.tomy.salminen.doublehelix.feature.mainfeed
 
 
+import fi.tomy.salminen.doublehelix.service.persistence.repository.ArticleRepository
 import fi.tomy.salminen.doublehelix.viewmodel.BaseViewModel
 import javax.inject.Inject
 
 
-class FeedActivityViewModel @Inject constructor() : BaseViewModel()
+class FeedActivityViewModel @Inject constructor(
+    articleRepository: ArticleRepository
+) : BaseViewModel() {
+
+    init {
+        compositeDisposable.add(
+            articleRepository.updateArticles().subscribe()
+        )
+    }
+}
