7434f09780e03dad25b442b8154d784334581309
Unknown <tomy_salminen@msn.com>
2020-04-26 15:58:58 +0300

    Fade images in

--- app/src/main/java/fi/tomy/salminen/doublehelix/binding/ImageViewBindings.kt
+++ app/src/main/java/fi/tomy/salminen/doublehelix/binding/ImageViewBindings.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
 
 @BindingAdapter(
@@ -14,7 +15,9 @@ import com.bumptech.glide.Glide
     requireAll = false
 )
 fun bindImageUrl(imageView: ImageView, url: String?, placeHolder: Drawable?) {
-    var builder = Glide.with(imageView.context).load(url)
+    var builder = Glide.with(imageView.context)
+        .load(url)
+        .transition(DrawableTransitionOptions.withCrossFade())
 
     if (placeHolder != null) {
         builder = builder.placeholder(placeHolder)
